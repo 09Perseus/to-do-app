1b2cf393d4e48266012e7904e1c839f5e54d6c87
09Perseus <bkumarevel@gmail.com>
2026-03-18 13:03:43 +0900

    Linked POSTGRE SQL database managed by supabase and made register, login, and authenticateToken functions in the backend

--- server/server.js
+++ server/server.js
@@ -5,12 +5,78 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const { Pool } = require("pg");
+const bcrypt = require("bcrypt");
+const jsonwebtoken = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET;
 
 app.use(cors());
 app.use(express.json());
 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
+//Middleware
+function authenticateToken(req, res, next) {
+  const token = req.headers["authorization"]?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ error: "No toen provided" });
+  }
+
+  try {
+    const user = jwt.verify(token, JWT_SECRET);
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(401).json({ error: "Invalid token" });
+  }
+}
+
 let todos = [];
 let nextId = 1;
+
+//Register to create an account
+app.post("/api/register", async (req, res) => {
+  const { username, password } = req.body;
+  const hPassword = await bcrypt.hash(password, 10);
+
+  try {
+    const result = await pool.query(
+      "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id, username",
+      [username, hPassword],
+    );
+    res.json(result.rows[0]);
+  } catch (err) {
+    res.status(400).json({ error: "Username already exists" });
+  }
+});
+
+//Logging in as a user
+app.post("/api/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  //Check the user
+  const result = await pool.query("SELECT * FROM users WHERE username = $1", [
+    username,
+  ]);
+  const user = result.rows[0];
+  if (!user) {
+    return res.status(401).json({ error: "Invalid username or password" });
+  }
+
+  //Check the password
+  const validPassword = bcrypt.compare(password, user.password);
+  if (!validPassword) {
+    return res.status(401).json({ error: "Invalid username or password" });
+  }
+
+  //Create a token for the user
+  const token = jwt.sign({ id: user.id }, JWT_SECRET);
+  res.json({ token });
+});
 
 //Request all the to-dos
 app.get("/api/todos", (req, res) => {
